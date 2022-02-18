8845420c5fb762c0a2c011708bdecc9f158f02f4
heismehrab <hedayatmanesh.mehrab@gmail.com>
2022-02-19 00:59:36 +0330

    customize pages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,9 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/logout', function () {
+    Auth::logout();
+
+    return redirect('/');
+})->name('logout');
